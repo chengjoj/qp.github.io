a897bf218144a9dfbc6f1fb4af599f10dd370f2f
chengjoj <2535427005@qq.com>
2024-12-20 01:52:57 +0800

    Update script3.js

--- script3.js
+++ script3.js
@@ -1,31 +1,32 @@
 document.addEventListener('DOMContentLoaded', function() {
     const bouncingContainer = document.querySelector('.bouncing-container');
     const bouncingCircle = document.querySelector('.bouncing-circle');
-
-    let posX = 0;
-    let posY = 0;
-    let speedX = 2;
-    let speedY = 2;
+    let posX = bouncingContainer.clientWidth / 2;
+    let posY = bouncingContainer.clientHeight / 2;
+    let speedX = 5;
+    let speedY = 5;
 
     function moveCircle() {
+        // 更新位置
         posX += speedX;
         posY += speedY;
 
-        // 检测左右边界碰撞
+        // 检测与容器边界的碰撞
         if (posX <= 0 || posX >= bouncingContainer.clientWidth - bouncingCircle.clientWidth) {
             speedX = -speedX;
         }
-
-        // 检测上下边界碰撞
         if (posY <= 0 || posY >= bouncingContainer.clientHeight - bouncingCircle.clientHeight) {
             speedY = -speedY;
         }
 
+        // 移动圆形
         bouncingCircle.style.left = `${posX}px`;
         bouncingCircle.style.top = `${posY}px`;
 
+        // 请求下一帧动画
         requestAnimationFrame(moveCircle);
     }
 
+    // 启动动画
     requestAnimationFrame(moveCircle);
 });
