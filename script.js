c6503f15452454cecb11df66f5fadf0a23f57907
chengjoj <2535427005@qq.com>
2024-12-20 00:07:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ document.addEventListener('DOMContentLoaded', function() {
     var backToTopButton = document.querySelector('.back-to-top');
     
     backToTopButton.addEventListener('click', function(event) {
-        event.preventDefault(); // 阻止默认行为
+        event.preventDefault(); // 阻止<a>标签的默认行为
         window.scrollTo({
-            top: 0, /* 滚动到页面顶部 */
-            behavior: 'smooth' /* 平滑滚动 */
+            top: 0, // 滚动到页面顶部
+            behavior: 'smooth' // 平滑滚动
         });
     });
 });
