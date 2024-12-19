f1233ee62a12529e8c4f07d68a47f59f1b7d5f9b
chengjoj <2535427005@qq.com>
2024-12-19 23:34:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+document.addEventListener('DOMContentLoaded', function() {
+    var backToTopButton = document.querySelector('.back-to-top');
+    
+    backToTopButton.addEventListener('click', function(event) {
+        event.preventDefault(); // 阻止默认行为
+        window.scrollTo({
+            top: 0, /* 滚动到页面顶部 */
+            behavior: 'smooth' /* 平滑滚动 */
+        });
+    });
+});
